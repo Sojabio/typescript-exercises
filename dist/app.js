4eddf6e42531378a7d6f9f0001bcbf8d179031bf
soja <solene.jabaud@gmail.com>
2023-11-23 11:34:58 +0100

    first commit

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+var compteur = document.querySelector('#compteur');
+var i = 0;
+var increment = function (e) {
+    e.preventDefault();
+    i++;
+    var span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector('span');
+    if (span) {
+        span.innerText = i.toString();
+        console.log('click');
+    }
+};
+compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+const compteur = document.querySelector('#compteur')
+let i = 0;
+
+const increment = (e: Event) => {
+  e.preventDefault()
+  i++;
+  const span = compteur?.querySelector('span')
+  if (span) {
+    span.innerText = i.toString()
+    console.log('click')
+  }
+}
+
+compteur?.addEventListener('click', increment)
